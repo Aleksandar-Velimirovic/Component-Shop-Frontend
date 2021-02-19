ffe7c59e62258ce1a9a186e53e049e61570f31af
Aleksandar Velimirovic <aleksandarvelimirovic2018@gmail.com>
2021-02-19 13:29:42 +0100

    server modified

--- service.js
+++ service.js
@@ -3,3 +3,8 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
+
+app.use('/', express.static(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
