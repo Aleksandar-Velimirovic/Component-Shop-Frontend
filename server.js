8bcd79783e1c51a4db1912404c74208caea92d76
Aleksandar Velimirovic <aleksandarvelimirovic2018@gmail.com>
2021-02-19 15:20:36 +0100

    json changed2

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path')
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use(express.static('static'));
 
 const port = process.env.PORT || 8080
 app.listen(port)
